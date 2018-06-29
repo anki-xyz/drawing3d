c29cf41b89fe2312c5816e6bf351b33ed4909cc4
anki-xyz <me@anki.xyz>
2018-06-29 14:02:04 +0200

    Re-named and reorganized repo.

--- setup.py
+++ setup.py
@@ -1,1 +1,9 @@
-# to do
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(name='drawing3d',
+      version='0.1',
+      author='Andreas Kist',
+      author_email='anki@neuro.mpg.de',
+      packages=find_packages(),
+      install_requires=['numpy', 'numba', 'scikit-image'])
